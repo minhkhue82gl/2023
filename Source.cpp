5366ddf3778ec46bbc527bf6fb8af0f75745945b
ludung06 <luquynhdung09@gmail.com>
2023-10-30 17:11:37 +0700

    them thuong func

--- Source.cpp
+++ Source.cpp
@@ -11,7 +11,9 @@ int tong(int a, int b){
 int tich(int a, int b){
      return a*b;
 }; 
-float thuong(int a, int b);
+float thuong(int a, int b){
+	return a*b;
+};
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
